fde59a53cb6160b22d84b89915fcc915ae776aa3
ndurbin <nick.durbin@gmail.com>
2019-12-17 06:41:27 -0500

    Completed the MVP for the query section.

--- queries.sql
+++ queries.sql
@@ -2,15 +2,27 @@
 
 -- Find all customers with postal code 1010
 
+  Cactus Comidas para llevar, Océano Atlántico Ltda., Rancho grande
+
 -- Find the phone number for the supplier with the id 11
+
+  (010) 9984510
 
 -- List first 10 orders placed, sorted descending by the order date
 
+  10443-10434
+
 -- Find all customers that live in London, Madrid, or Brazil
+
+  4, 8, 11, 15, 16, 19, 21, 22, 31, 34, 53, 61, 62, 67, 72, 81, 88
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
+  Done.
+
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+
+  Done.
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
